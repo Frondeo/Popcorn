150247d7d16329c3a825f50ef69cb751fa1859cd
andrey <me@messus.ru>
2024-06-07 18:52:45 +0700

    add massive and init()

--- Popcorn/Main.cpp
+++ Popcorn/Main.cpp
@@ -10,6 +10,21 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
+enum EBrick_Type
+{
+   ET_None,
+   ET_Red,
+   ET_Blue
+};
+enum EBall_Color
+{
+
+
+
+};
+HPEN  Brick_Red_Pen, Brick_Blue_Pen;
+HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
+
 const int Gl_scale = 3;
 const int Brick_Width = 15;
 const int Brick_Height = 7;
@@ -17,6 +32,22 @@ const int Cell_Width = 16;
 const int Cell_Height = 8;
 const int X_Offset = 8;
 const int Y_Offset = 6;
+char Level_one[14][12] =
+{  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+   2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+   2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+   2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+   2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -80,7 +111,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_POPCORN));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = CreateSolidBrush(RGB(0, 0, 0));
+    wcex.hbrBackground  = CreateSolidBrush(RGB(15, 63, 31));
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_POPCORN);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -96,13 +127,23 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
+// 
+//-------------------------------------------------------------------------
+void Init()
+{
+   Brick_Red_Pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 85));
+   Brick_Red_Brush = CreateSolidBrush(RGB(255, 85, 85));
 
+   Brick_Blue_Pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 255));
+   Brick_Blue_Brush = CreateSolidBrush(RGB(255, 85, 255));
+}
 
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
  
    //--------------------------------------------------------------------------
+   Init();
 
    RECT window_rect;
    window_rect.left = 0;
@@ -125,35 +166,48 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    return TRUE;
 }
+
 //--------------------------------------------------------------------------
-void Draw_Brick(HDC hdc, int x, int y, bool is_blue)
+void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type)
 {
-   HPEN  pen;
+   HPEN pen;
    HBRUSH brush;
 
-   if (is_blue)
+   switch (brick_type)
    {
-      pen = CreatePen(PS_SOLID, 0, RGB(85, 255, 255));
-      brush = CreateSolidBrush(RGB(85, 255, 255));
-   }
-   else
-   {
-      pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 255));
-      brush = CreateSolidBrush(RGB(255, 85, 255));
-   }
+      case ET_None:
+         return;
+
+      case ET_Red:
+         pen = Brick_Red_Pen;
+         brush = Brick_Red_Brush;
+         break;
+
+      case ET_Blue:
+         pen = Brick_Blue_Pen;
+         brush = Brick_Blue_Brush;
+         break;
+
+      default:
+         return;
+   } 
 
    SelectObject(hdc, pen);
    SelectObject(hdc, brush);
-   Rectangle(hdc, x * Gl_scale, y * Gl_scale, (x + Brick_Width) * Gl_scale, (y + Brick_Height) * Gl_scale);
+   RoundRect(hdc, x * Gl_scale, y * Gl_scale, (x + Brick_Width) * Gl_scale, (y + Brick_Height) * Gl_scale, 2*Gl_scale, 2*Gl_scale);
 }
-
 //--------------------------------------------------------------------------
-void Draw_Frame(HDC hdc)
+void Draw_Level(HDC hdc)
 {
    int i, j;
    for (i = 0; i < 14; i++)
       for (j = 0; j < 12; j++)
-         Draw_Brick(hdc, X_Offset + j * Cell_Width, Y_Offset + i * Cell_Height, true);
+         Draw_Brick(hdc, X_Offset + j * Cell_Width, Y_Offset + i * Cell_Height, (EBrick_Type)Level_one[i][j]);
+}
+//--------------------------------------------------------------------------
+void Draw_Frame(HDC hdc)
+{
+   Draw_Level(hdc);
 }
 //--------------------------------------------------------------------------
 
